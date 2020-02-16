e82f7b06f1586d6e2aedfa7a53d31e055a40855a
andres <condeandres34@gmail.com>
2020-02-16 16:31:22 -0300

    base de datos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{'CRYSIS'}}</title>
+    <title>{{'NotiNews'}}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
